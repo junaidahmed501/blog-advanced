83be84574818bf6336ba2e76bfc90fb104c049f0
Junaid Ahmed <junaidahmed501@gmail.com>
2021-03-07 08:05:58 +0500

    Adds env files & updates the relative files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
 const urljoin = require("url-join");
 const path = require("path");
 const config = require("./data/SiteConfig");
